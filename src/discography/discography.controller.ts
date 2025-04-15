68a6daf214a27acf5f31b9d67bedf2a6e0bd802a
DOOYEE0709 <hyeonta03@gmail.com>
2025-04-16 01:55:03 +0900

    🔐 feat: Apply ApiKeyAndIpAuthGuard to Discography write routes

--- src/discography/discography.controller.ts
+++ src/discography/discography.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Post, Put, Delete, Param, Body, Patch } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, Patch, UseGuards } from '@nestjs/common';
 import { DiscographyService } from './discography.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateDiscographyDto, UpdateDiscographyDto } from './dto/discography.dto';
 import { DiscographyResponseDto } from './dto/discography.dto';
+import { ApiKeyAndIpAuthGuard } from 'src/whitelist-ip/api-key-ip-auth.guard';
 
 @ApiTags('Discography')
 @Controller('discography')
@@ -21,6 +22,7 @@ export class DiscographyController {
     return this.discographyService.findOne(id);
   }
 
+  @UseGuards(ApiKeyAndIpAuthGuard)
   @Post()
   @ApiOperation({ summary: 'Create a discography record' })
   async create(
@@ -29,6 +31,7 @@ export class DiscographyController {
     return this.discographyService.create(discographyData);
   }
 
+  @UseGuards(ApiKeyAndIpAuthGuard)
   @Patch(':id')
   @ApiOperation({ summary: 'Update a discography record based on ID' })
   async update(
@@ -38,6 +41,7 @@ export class DiscographyController {
     return this.discographyService.update(id, updateData);
   }
 
+  @UseGuards(ApiKeyAndIpAuthGuard)
   @Delete(':id')
   @ApiOperation({ summary: 'Delete a discography record based on ID' })
   async delete(@Param('id') id: string): Promise<DiscographyResponseDto> {
